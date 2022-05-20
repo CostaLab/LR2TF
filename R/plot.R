ebe68462526892dd038453dabf5869dbd51efcca
Vanessa <vanessa.kraemer1@online.de>
2022-05-20 14:11:21 +0200

    made plot functions avaiable

--- R/plot.R
+++ R/plot.R
@@ -8,6 +8,7 @@
 #' @import ComplexHeatmap
 #' @import ggplot2
 #' @import grid
+#' @export
 plot_condition_tf_activities <-
   function(tf_activity_tables, out_path) {
 
@@ -58,6 +59,7 @@ plot_condition_tf_activities <-
 #' @import ComplexHeatmap
 #' @import ggplot2
 #' @import grid
+#' @export
 plot_condition_tf_activities_compressed <-
   function(tf_activity_tables, out_path) {
     tmp_out_path = paste0(out_path, "/tmp")
