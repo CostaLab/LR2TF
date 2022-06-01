4bc448df041f45095ad04dcd33246a06ff9634cc
Vanessa <vanessa.kraemer1@online.de>
2022-06-01 11:50:19 +0200

    error catching for unvalid index

--- R/get_significant_tfs.R
+++ R/get_significant_tfs.R
@@ -8,7 +8,7 @@
 #' @param tf_condition_significant condition comparison results
 #' @return A data frame with transcription factor activity scores per cell type
 #' @export
-get_significant_tfs <- function(seuratobject, condition, out_path, tf_condition_significant){
+get_significant_tfs <- function(seuratobject, condition, out_path, tf_condition_significant) {
   single_result_path <- paste0(out_path, '/', condition)
   dir.create(single_result_path)
 
@@ -23,11 +23,11 @@ get_significant_tfs <- function(seuratobject, condition, out_path, tf_condition_
                                          min.pct = 0, logfc.threshold = 0,
                                          verbose = FALSE)
   write.csv(seuratobject.markers, file =
-              paste0(single_result_path,'/all_specificmarker_',
-                     '_',condition,'.csv'))
+    paste0(single_result_path, '/all_specificmarker_',
+           '_', condition, '.csv'))
 
   seuratobject.markers$tag <- sapply(seuratobject.markers$p_val_adj, function(pval) {
-    if(pval< 0.001) {
+    if (pval < 0.001) {
       txt <- "***"
     } else if (pval < 0.01) {
       txt <- "**"
@@ -40,7 +40,7 @@ get_significant_tfs <- function(seuratobject, condition, out_path, tf_condition_
   })
 
   seuratobject.markers$log_fc_tag <- sapply(seuratobject.markers$avg_log2FC, function(log_fc) {
-    if(log_fc >= 1.0) {
+    if (log_fc >= 1.0) {
       txt <- "***"
     } else if (log_fc > 0.5) {
       txt <- "**"
@@ -52,7 +52,7 @@ get_significant_tfs <- function(seuratobject, condition, out_path, tf_condition_
     return(txt)
   })
 
-  tag_mapping = seuratobject.markers[c("gene","tag", "log_fc_tag", "cluster")]
+  tag_mapping = seuratobject.markers[c("gene", "tag", "log_fc_tag", "cluster")]
   tag_mapping = tag_mapping[(tag_mapping$tag == "***"),]
   tag_mapping = tag_mapping[(tag_mapping$log_fc_tag == "***"),]
   tag_mapping = dcast(tag_mapping, gene ~ cluster, value.var = "tag")
@@ -75,9 +75,9 @@ get_significant_tfs <- function(seuratobject, condition, out_path, tf_condition_
   unfiltered_viper_scores = unfiltered_tf_activity_table(viper_scores_df, CellsClusters)
 
   col.num <- which(colnames(viper_scores_df) %in% rownames(tag_mapping))
-  viper_scores_df <- viper_scores_df[,sort(c(col.num))]
+  viper_scores_df <- viper_scores_df[, sort(c(col.num))]
 
-  viper_scores_clusters <- viper_scores_df  %>%
+  viper_scores_clusters <- viper_scores_df %>%
     data.frame() %>%
     rownames_to_column("cell") %>%
     gather(tf, activity, -cell) %>%
@@ -96,7 +96,7 @@ get_significant_tfs <- function(seuratobject, condition, out_path, tf_condition_
   summarized_viper_scores_df = t(summarized_viper_scores_df)
 
   tf_scores_variable_table = save_variable_tf_scores(summarized_viper_scores, condition, single_result_path)
-  write.csv(summarized_viper_scores_df, file = paste0(single_result_path,'/tf_scores','_',condition,'.csv'))
+  write.csv(summarized_viper_scores_df, file = paste0(single_result_path, '/tf_scores', '_', condition, '.csv'))
 
   message("Plotting top variables tf activities")
   plot_highly_variable_tfs(summarized_viper_scores, condition,
@@ -106,35 +106,40 @@ get_significant_tfs <- function(seuratobject, condition, out_path, tf_condition_
   plot_tf_activity(summarized_viper_scores_df, tag_mapping, condition,
                    single_result_path)
 
-  rownames(summarized_viper_scores_df) = gsub(".", "-", rownames(summarized_viper_scores_df), fixed=TRUE)
+  rownames(summarized_viper_scores_df) = gsub(".", "-", rownames(summarized_viper_scores_df), fixed = TRUE)
   seuratobject.markers = seuratobject.markers[(seuratobject.markers$tag == "***"),]
   seuratobject.markers = seuratobject.markers[(seuratobject.markers$log_fc_tag == "***"),]
 
-  map_z_value_filtered <- function(gene, cluster){
-    z_score = summarized_viper_scores_df[as.character(gene), as.character(cluster)]
-    return(z_score)
+  map_z_value_filtered <- function(gene, cluster) {
+    if (gene %in% rownames(summarized_viper_scores_df)) {
+      z_score = summarized_viper_scores_df[as.character(gene), as.character(cluster)]
+      return(z_score)
+    } else {
+      return(NA)
+    }
   }
 
-  map_z_value <- function(gene, cluster){
-    if (gene %in% rownames(unfiltered_viper_scores)){
+  map_z_value <- function(gene, cluster) {
+    if (gene %in% rownames(unfiltered_viper_scores)) {
       z_score = unfiltered_viper_scores[as.character(gene), as.character(cluster)]
       return(z_score)
-    } else{
+    } else {
       return(NA)
     }
   }
 
   res <- list()
   seuratobject.markers$z_score = mapply(map_z_value_filtered, seuratobject.markers$gene, seuratobject.markers$cluster)
-  seuratobject.markers = seuratobject.markers[!(seuratobject.markers$tag=="ns"),]
-  res[["cluster"]] = seuratobject.markers[c("gene","tag", "cluster", "z_score")]
-  write.csv(res[["cluster"]], file = paste0(single_result_path,'/significant_cluster_tf_results','_',condition,'.csv'))
+  seuratobject.markers = seuratobject.markers[!(seuratobject.markers$tag == "ns"),]
+  seuratobject.markers = na.omit(seuratobject.markers)
+  res[["cluster"]] = seuratobject.markers[c("gene", "tag", "cluster", "z_score")]
+  write.csv(res[["cluster"]], file = paste0(single_result_path, '/significant_cluster_tf_results', '_', condition, '.csv'))
 
-  tf_condition_significant$gene = gsub(".", "-", tf_condition_significant$gene, fixed=TRUE)
+  tf_condition_significant$gene = gsub(".", "-", tf_condition_significant$gene, fixed = TRUE)
   tf_condition_significant$z_score = mapply(map_z_value, tf_condition_significant$gene, tf_condition_significant$cluster)
   tf_condition_significant = na.omit(tf_condition_significant)
   res[["condition"]] = tf_condition_significant
-  write.csv(res[["condition"]], file = paste0(single_result_path,'/significant_condition_tf_results','_',condition,'.csv'))
+  write.csv(res[["condition"]], file = paste0(single_result_path, '/significant_condition_tf_results', '_', condition, '.csv'))
 
   return(res)
 }
