8c1c8e3c7bf5166ebb5d4b25b95571b255671a7b
vckraemer <vanessa.kraemer1@online.de>
2024-02-21 13:31:02 +0100

    double checking for duplicated interactions when combining interaction types

--- R/utils.R
+++ R/utils.R
@@ -339,6 +339,7 @@ combine_LR_and_TF <- function(tf_table, LR_prediction, out_path, condition, add_
   if (add_node_type) {
     complete_interactions <- add_node_type(complete_interactions)
   }
+  complete_interactions <- unique(complete_interactions)
   write.csv(complete_interactions, paste0(out_path, "CrossTalkeR_input_", condition, ".csv"), row.names = FALSE)
   return(complete_interactions)
 }
