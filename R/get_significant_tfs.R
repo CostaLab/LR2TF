4a8f9cc91298f540093d2df66058d23a63c240ad
vckraemer <vanessa.kraemer1@online.de>
2024-12-09 13:15:58 +0100

    fixing tf score filtering by row index

--- R/get_significant_tfs.R
+++ R/get_significant_tfs.R
@@ -89,7 +89,7 @@ get_significant_tfs <- function(seuratobject, condition, out_path, pval, log2fc,
   write.csv(summarized_tf_scores_df, file = paste0(single_result_path, '/unfiltered_tf_scores', '_', condition, '.csv'))
 
   col.num <- which(rownames(summarized_tf_scores_df) %in% rownames(tag_mapping))
-  filtered_tf_scores_df <- summarized_tf_scores_df[, sort(c(col.num))]
+  filtered_tf_scores_df <- summarized_tf_scores_df[sort(c(col.num)), ]
   write.csv(filtered_tf_scores_df, file = paste0(single_result_path, '/tf_scores', '_', condition, '.csv'))
 
   tf_scores_variable_table <- save_variable_tf_scores(filtered_tf_scores_df, condition, single_result_path)
